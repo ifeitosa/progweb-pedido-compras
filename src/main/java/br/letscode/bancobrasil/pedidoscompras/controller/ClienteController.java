3811a29c6c0d5c13fed42641c4b23a96ed1b6134
Rodrigo Raminelli <rraminelli@gmail.com>
2022-08-01 23:35:43 +0200

    spring data

--- src/main/java/br/letscode/bancobrasil/pedidoscompras/controller/ClienteController.java
+++ src/main/java/br/letscode/bancobrasil/pedidoscompras/controller/ClienteController.java
@@ -1,19 +1,33 @@
 package br.letscode.bancobrasil.pedidoscompras.controller;
 
 import br.letscode.bancobrasil.pedidoscompras.model.Cliente;
+import br.letscode.bancobrasil.pedidoscompras.repository.ClienteRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.tomcat.util.http.MimeHeaders;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.MimeType;
+import org.springframework.util.TypeUtils;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.time.LocalDate;
+import java.util.Optional;
 
 @Slf4j
 @RestController
 @RequestMapping("/cliente")
 public class ClienteController {
 
+    @Autowired
+    ClienteRepository clienteRepository;
+
     @PostMapping
-    public ResponseEntity<Cliente> save(@RequestBody Cliente cliente) {
+    public ResponseEntity<Cliente> save(@RequestBody @Valid Cliente cliente) {
         log.debug("Chegou a request do cliente: " + cliente.toString());
-        return null;
+        return ResponseEntity.ok(clienteRepository.save(cliente));
     }
 
 }
--- src/main/java/br/letscode/bancobrasil/pedidoscompras/model/Cliente.java
+++ src/main/java/br/letscode/bancobrasil/pedidoscompras/model/Cliente.java
@@ -5,18 +5,40 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
 @Getter
 @Setter
 @EqualsAndHashCode
 @ToString
+@Entity
 public class Cliente {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotNull(message = "Nome nao pode ser null")
+    @NotEmpty(message = "Nome nao pode ser vazio")
     private String nome;
+
+    @NotNull(message = "CPF nao pode ser null")
+    @NotEmpty(message = "CPF nao pode ser vazio")
     private String cpf;
+
+    @NotNull(message = "Email nao pode ser null")
+    @NotEmpty(message = "Email nao pode ser vazio")
+    @Email(message = "Email invalido")
     private String email;
+
     private String senha;
 
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "endereco_id", referencedColumnName = "id")
     private Endereco endereco;
 
 }
